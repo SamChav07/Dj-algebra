e91e4eb3a9230434a4f7ae29d3d0003abef0a9e2
SamChav07 <schavarriab@uamv.edu.ni>
2024-09-26 10:30:31 -0600

    upt_general

--- my_algebra_calculator/eliminacionGauss/admin.py
+++ my_algebra_calculator/eliminacionGauss/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Elim_Gauss)
+class EG_admin(admin.ModelAdmin):
+    list_display = ('EG_fila', 'EG_columna', 'EG_valor',)
+admin.site.register(Elim_Gauss, EG_admin)
