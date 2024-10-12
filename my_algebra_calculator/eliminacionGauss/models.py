f07043541283d27c189cdd27adad728589787c09
SamChav07 <schavarriab@uamv.edu.ni>
2024-10-11 18:10:05 -0600

    Upt_model

--- my_algebra_calculator/eliminacionGauss/models.py
+++ my_algebra_calculator/eliminacionGauss/models.py
@@ -2,38 +2,28 @@
 
 from django.db import models
 
+#Modelo del metodo escalonado
 class Elim_Gauss(models.Model):
-    EG_matriz = models.JSONField()  # Asegúrate de que esta línea esté presente
-    EG_tabla_id = models.IntegerField()  # ID de la tabla relacionada
+    EG_matriz = models.JSONField()  #Guarda la matriz completa 
     EG_resultado = models.JSONField(null=True, blank=True)  # Resultado de la eliminación gaussiana
-    EG_ecuaciones = models.TextField(null=True, blank=True)  # Asegúrate de que esta línea esté presente
-
+    EG_ecuaciones = models.TextField(null=True, blank=True)  # Guarda los procedimientos
     def __str__(self):
-        return f"Elim_Gauss {self.id} - Tabla ID: {self.EG_tabla_id}"
+        return f"Elim_Gauss {self.id}"
 
+#Modelo del metodo operaciones combinadas de vectores
 class Ope_combinadas(models.Model):
-    OpV_NmVectores = models.IntegerField()# Columnas X
-    OpV_DmVectores = models.IntegerField()# Filas Y
-    OpV_valor = models.FloatField()
-    class Meta:
-        unique_together = ('OpV_NmVectores', 'OpV_DmVectores')
+    OpV_vectores = models.JSONField() #vectores
+    OpV_escalares = models.JSONField()# Escalares en funcion a n OpV_NmVectores
+    OpV_resultado = models.JSONField()
+    OpV_ecuaciones = models.TextField()
     def __str__(self):
-        return f"({self.OpV_NmVectores}, {self.OpV_DmVectores}): {self.OpV_valor}"
+        return f"Ope_combinadas {self.id}"
 
-class MltFC_horizontal(models.Model):
-    MfcX_NmVectorX = models.IntegerField()
-    MfcX_NmVectorY = models.IntegerField()
-    MfcX_valor = models.FloatField()
-    class Meta:
-        unique_together = ('MfcX_NmVectorX', 'MfcX_NmVectorY')
+#Modelo del metodo multiplicacion vector fila x columna
+class MultiFxC(models.Model):
+    Mfc_Fila = models.JSONField()  
+    Mfc_Column = models.JSONField()
+    Mfc_resultado = models.JSONField()
+    Mfc_ecuaciones = models.TextField()
     def __str__(self):
-        return f"({self.MfcX_NmVectorX}, {self.MfcX_NmVectorY}): {self.MfcX_valor}"
-
-class MltFC_vertical(models.Model):
-    MfcY_NmVectorX = models.IntegerField()
-    MfcY_NmVectorY = models.IntegerField()
-    MfcY_valor = models.FloatField()
-    class Meta:
-        unique_together = ('MfcY_NmVectorX', 'MfcY_NmVectorY')
-    def __str__(self):
-        return f"({self.MfcX_NmVectorX}, {self.MfcX_NmVectorY}): {self.MfcX_valor}"
+        return f"MultiFxC {self.id}"
