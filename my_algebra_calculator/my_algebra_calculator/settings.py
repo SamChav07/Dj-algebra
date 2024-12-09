2dd14453d0fb96147fc854747af3b629e13c3b6b
SamChav07 <127228834+SamChav07@users.noreply.github.com>
2024-12-09 16:57:23 -0600

    Update settings.py

--- my_algebra_calculator/my_algebra_calculator/settings.py
+++ my_algebra_calculator/my_algebra_calculator/settings.py
@@ -38,7 +38,7 @@ ROOT_URLCONF = 'my_algebra_calculator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
